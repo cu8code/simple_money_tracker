7833b84b5bd9d09ad62c3d325fdd17137b47b1bf
cu8code <git8ankanroy@gmail.com>
2022-12-18 23:46:39 +0530

    fall guy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
+import { ChartData, ChartOptions } from "chart.js";
+import { Line } from "react-chartjs-2";
+
+import Titlebar from "../components/titleBar/titleBar";
+
+const line: ChartData<"line", any, unknown> = {
+  labels: ["a", "b", "c"],
+  datasets: [{ label: "anakn", data: 9 }],
+};
+
+function RecentAcitivity() {
+  return <div className="flex fex-col h-20 w-full mt-10 bg-slate-400"></div>;
+}
+
+function Graph() {
+  return (
+    <canvas className="flex flex-col h-20 w-full mt-10 bg-slate-300"></canvas>
+  );
+}
+
 export default function Home() {
   return (
     <>
-      <h1>Ankan Roy</h1>
+      <Titlebar />
+      <Line options={{}} data={line} />
     </>
   );
 }
