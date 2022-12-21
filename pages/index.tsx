aeb4088db5317bfe23e4f8e7785e544d6512e6bc
cu8code <git8ankanroy@gmail.com>
2022-12-21 09:49:49 +0530

    how to make a github

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,19 @@
-import { Line } from "react-chartjs-2"
 import { Chart, registerables } from "chart.js"
-
+import { useRef } from "react";
 import Titlebar from "../components/titleBar/titleBar";
-import { Datahandeler,  getLabel, getUserDataPoint } from "../module/dataHandeler";
 
 Chart.register(...registerables)
 
-const datahandeler = Datahandeler(1000, 10)
-
-function RecentAcitivity(props: { chartData: number[] }) {
-  return <div className="flex fex-col h-20 w-full mt-10 bg-slate-400"></div>;
-}
-
-function Graph() {
-  const data = {
-    labels: getLabel(datahandeler),
-    datasets: [
-      {
-        label: "First dataset",
-        data: getUserDataPoint(datahandeler),
-        fill: true,
-        backgroundColor: "rgba(75,192,192,0.2)",
-        borderColor: "rgba(75,192,192,1)"
-      },
-      {
-        label: "Second dataset",
-        data: [33, 25, 35, 51, 54, 76],
-        fill: false,
-        borderColor: "#742774"
-      }
-    ]
-  };
-  return (
-    <Line data={data} />
-  );
-}
 
 export default function Home() {
+  const button = useRef<HTMLButtonElement>(null)
   return (
     <>
       <Titlebar />
       <div className="w-full max-w-2xl m-auto flex sm:py-5 px-1">
-        <Graph />
+        <input type="text" className="border"></input>
       </div>
+      <button ref={button} className="w-1/3 m-auto flex ">SAVE</button>
     </>
   );
 }
