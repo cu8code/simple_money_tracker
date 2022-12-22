4fd0ce9bc84018150114360acca99b970202d7be
cu8code <git8ankanroy@gmail.com>
2022-12-23 00:31:03 +0530

    some thing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
-import { onAuthStateChanged } from "firebase/auth";
 import { Chart, registerables } from "chart.js"
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-import { AuthSinIn, AuthSinUp } from "../components/auth/";
+import { AuthSinIn } from "../components/auth/";
 import Titlebar from "../components/titleBar/";
 
 Chart.register(...registerables)
@@ -25,20 +24,21 @@ function Elo() {
       { /* net wprth */}
       <div className="flex flex-col w-full p-6 text-gray-700 " >
         <div className="flex w-full justify-between border-b-2 py-1">
-          <h1 className="mb-2 text-3xl font-bold tracking-tight ">Net Worth</h1>
+          <h1 className="mb-2 text-3xl font-bold tracking-tight ">Balance</h1>
           <h1 className="mb-2 text-3xl font-bold tracking-tight text-green-500">💸18200</h1>
         </div>
         <div>
+          <div className="flex w-full justify-center bg-slate-100 border-b-2 p-1"> Latest Money Recived </div>
           <div className="flex w-full justify-between bg-slate-100 border-b-2 p-1">
-            <div>****112 (default)</div>
+            <div>Fir Dec 23 00:23</div>
             <div className="text-green-500 font-bold">100</div>
           </div>
           <div className="flex w-full justify-between border-b-2 p-1">
-            <div>****152</div>
+            <div>Fir Dec 20 00:23</div>
             <div className="text-green-500 font-bold">100</div>
           </div>
           <div className="flex w-full justify-between bg-slate-100 border-b-2 p-1">
-            <div>****119</div>
+            <div>Fir Dec 15 00:23</div>
             <div className="text-green-500 font-bold">18000</div>
           </div>
         </div>
